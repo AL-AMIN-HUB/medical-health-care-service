0d635858d4de0d8d9c9dd1471f71cd07b840e2a4
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-18 11:36:23 +0600

    added services section

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Card, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const Service = () => {
+const Service = (props) => {
+  const { name, image, desc } = props.service;
   return (
     <div>
-      <h1>Service</h1>
+      <Col>
+        <Card className="h-100 pb-4">
+          <Card.Img variant="top" className="img-fluid" src={image} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{desc.slice(0, 80)}...</Card.Text>
+          </Card.Body>
+          <Button className="btn button-active-color border-0 w-50 mx-auto fs-4 py-2  rounded-pill btn-hover">
+            <Link className="text-decoration-none text-white" to="/serviceDetails">
+              {" "}
+              View Details
+            </Link>{" "}
+          </Button>
+        </Card>
+      </Col>
     </div>
   );
 };
--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
+import { Row } from "react-bootstrap";
+import Service from "../Service/Service";
 import "./Services.css";
 
 const Services = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/9eb8828a-6692-499d-a7d8-ddfac1fc0894")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  }, []);
   return (
     <div className="services-bg py-5">
       <div className="text-center service-title">
@@ -12,7 +22,13 @@ const Services = () => {
         </p>
       </div>
 
-      <div className="row container"></div>
+      <div>
+        <Row xs={1} sm={1} md={3} className="g-4 w-75 mx-auto mt-5">
+          {services.map((service) => (
+            <Service key={service.id} service={service}></Service>
+          ))}
+        </Row>
+      </div>
     </div>
   );
 };
