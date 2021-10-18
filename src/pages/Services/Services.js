11d52bf6a35fea07b67947de5c6e5f53d45e08bd
Md-Alamin-hub <86272640+Md-Alamin-hub@users.noreply.github.com>
2021-10-18 10:51:59 +0600

    added services

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,9 +1,18 @@
-import React from "react";
+import "./Services.css";
 
 const Services = () => {
   return (
-    <div>
-      <h1>Services</h1>
+    <div className="services-bg py-5">
+      <div className="text-center service-title">
+        <h5 className="text-custom-primary my-5">Our Services</h5>
+        <h3 className="text-custom-secondary">Medcity professional services</h3>
+        <p className="text-muted">
+          Why Medcity services is best all time since 1990. desires to obtain of itself, because it is pain, <br /> but because occasionally
+          circumstance procure him some great deals.
+        </p>
+      </div>
+
+      <div className="row container"></div>
     </div>
   );
 };
